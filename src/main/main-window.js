e563debc93b73f91b411b39f662f29eccd53a56d
tatsuya itakura <itkrt2y.591721200@gmail.com>
2015-11-23 23:59:37 +0900

    create speech recognition sample

--- src/main/main-window.js
+++ src/main/main-window.js
@@ -2,7 +2,7 @@ import { BrowserWindow } from 'electron';
 
 export default class MainWindow {
   constructor() {
-    this.window = new BrowserWindow({ width: 1200, height: 800 });
+    this.window = new BrowserWindow({ width: 800, height: 600 });
     this.window.loadURL(`file://${__dirname}/../renderer/index.html`);
     if (process.env.NODE_ENV === 'development') {
       this.window.webContents.openDevTools();
--- src/renderer/components/main.js
+++ src/renderer/components/main.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 export default class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.recognition = this.initializeSpeechRecognition();
+  }
+
+  initializeSpeechRecognition() {
+    window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+    let recognition = new webkitSpeechRecognition();
+    recognition.lang = 'ja';
+
+    // 録音終了時トリガー
+    recognition.addEventListener('result', (event) => {
+      console.log(event.results.item(0).item(0).transcript)
+    }, false);
+
+    return recognition;
+  }
+
   speech() {
-    speechSynthesis.speak(
-      new SpeechSynthesisUtterance("Hello World!")
-    );
+    // 録音開始
+    this.recognition.start();
   }
 
   render() {
